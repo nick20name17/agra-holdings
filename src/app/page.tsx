0ad7e44cdd0c86e1ab5681ba15016dda4093e975
nazar <nick20name17@gmail.com>
2023-11-12 00:32:04 +0200

    add pages title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
 import { GeneralInfo } from 'components/GeneralInfo/GeneralInfo'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'Agra Holdings',
+	description: 'Agra Holdings',
+	icons: {
+		icon: './favicon.svg'
+	}
+}
 
 export default function Home() {
 	return (
