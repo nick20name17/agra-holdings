e1fbb615e657eadb6ee3b1f9bacc1271463248b1
nazar <nick20name17@gmail.com>
2023-11-13 04:00:40 -0800

    update button component

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -2,8 +2,21 @@ import { PropsWithChildren } from 'react'
 
 import styles from './Button.module.scss'
 
-interface ButtonProps extends PropsWithChildren {}
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	icon?: React.ReactNode
+}
 
-export const Button: React.FC<ButtonProps> = ({ children }) => {
-	return <button className={styles.btn}>{children}</button>
+export const Button: React.FC<ButtonProps> = ({
+	children,
+	icon = '',
+	...attrs
+}) => {
+	const btnClasses = `${styles.btn} ${icon ? styles.btn_icon : ''}`
+
+	return (
+		<button className={btnClasses} {...attrs}>
+			{icon}
+			{children}
+		</button>
+	)
 }
